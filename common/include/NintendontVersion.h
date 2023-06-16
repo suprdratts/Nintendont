668c83bb6c7b8109fbce5a22c99b5200bbe925f2
Jas Laferriere <Fizzi36@gmail.com>
2023-06-15 22:52:41 -0400

    release: v1.11.1

--- common/include/NintendontVersion.h
+++ common/include/NintendontVersion.h
@@ -3,6 +3,6 @@
 
 // Version number of Nintendont, doesn't show up in the app though, that requires you to push
 // the release tag on github via git push --atomic origin <branch name> <tag>
-#define NIN_VERSION "1.11.0"
+#define NIN_VERSION "1.11.1"
 
 #endif
