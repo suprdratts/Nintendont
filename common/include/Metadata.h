594fb6d24cebc2302ec72d80d0baf5343e313e11
jmlee337 <jmlee337@gmail.com>
2024-03-09 15:43:18 -0800

    chore: attribution updates (#41)

--- common/include/Metadata.h
+++ common/include/Metadata.h
@@ -3,7 +3,7 @@
 #define __META_DATA_H__
 
 #define META_NAME	"Nintendont Slippi"
-#define META_AUTHOR	"FIX94, crediar, Fizzi, metaconstruct, jmlee337, metonyms"
+#define META_AUTHOR	"FIX94, crediar, Fizzi, metaconstruct, Nicolet, nikki"
 
 #define META_LONG1	"A fork of Nintendont, designed specifically for Super Smash Bros. Melee."
 #define META_LONG2	"https://github.com/project-slippi/Nintendont\r\n\r\nhttps://slippi.gg"
